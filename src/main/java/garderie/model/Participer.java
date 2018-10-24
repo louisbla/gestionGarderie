630a7fefeada3b400682d269544e2aa41ab629eb
Joseph Lurel <joseph.lurel@gmail.com>
2018-10-24 19:42:13 -0400

    Ajout classe Participer

---
+++ src/main/java/garderie/model/Participer.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garderie.model;
+
+import java.util.*;
+
+/**
+ *
+ * @author Katsuo
+ */
+public class Participer {
+    private Groupe groupe;
+    private Salle salle;
+    private Activite activite;
+    private Date date;
+
+    public Participer(Groupe groupe, Salle salle, Activite activite, Date date) {
+        this.groupe = groupe;
+        this.salle = salle;
+        this.activite = activite;
+        this.date = date;
+    }
+
+    public Groupe getGroupe() {
+        return groupe;
+    }
+
+    public void setGroupe(Groupe groupe) {
+        this.groupe = groupe;
+    }
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public void setSalle(Salle salle) {
+        this.salle = salle;
+    }
+
+    public Activite getActivite() {
+        return activite;
+    }
+
+    public void setActivite(Activite activite) {
+        this.activite = activite;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+}
