49d07134de96c063a59dfa9b7bcd359cdce5eca2
Unknown <maryline.yakan@gmail.com>
2018-10-25 00:49:07 +0200

    Ajout ContactUrgence et enumeration groupe

---
+++ src/main/java/garderie/model/ContactUrgence.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garderie.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Maryline
+ */
+public class ContactUrgence extends Personne {
+    private String numTel;
+    private String lien;
+
+    public ContactUrgence(String numTel, String lien, String nom, String prenom, String sexe, Date dateNaissance, String numSecu, ArrayList<Adresse> adresses) {
+        super(nom, prenom, sexe, dateNaissance, numSecu, adresses);
+        this.numTel = numTel;
+        this.lien = lien;
+    }
+
+    public String getNumTel() {
+        return this.numTel;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
+    }
+
+    public String getLien() {
+        return this.lien;
+    }
+
+    public void setLien(String lien) {
+        this.lien = lien;
+    }
+    
+    
+}
---
+++ src/main/java/garderie/model/Groupe.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garderie.model;
+import garderie.model.*;
+/**
+ *
+ * @author Maryline
+ */
+public class Groupe {
+    private int idGroupe; 
+    private NomGroupe nom;
+    private int nbEnfant;
+    private String descriptif;
+    private Employe referant;
+
+    public Groupe(NomGroupe nom, int nbEnfant, String descriptif, Employe referant) {
+        this.nom = nom;
+        this.nbEnfant = nbEnfant;
+        this.descriptif = descriptif;
+        this.referant = referant;
+    }
+ 
+    
+    public int getIdGroupe() {
+        return idGroupe;
+    }
+
+    public NomGroupe getNom() {
+        return nom;
+    }
+
+    public void setNom(NomGroupe nom) {
+        this.nom = nom;
+    }
+
+    public int getNbEnfant() {
+        return nbEnfant;
+    }
+
+    public void setNbEnfant(int nbEnfant) {
+        this.nbEnfant = nbEnfant;
+    }
+
+    public String getDescriptif() {
+        return descriptif;
+    }
+
+    public void setDescriptif(String descriptif) {
+        this.descriptif = descriptif;
+    }
+
+    public Employe getReferant() {
+        return referant;
+    }
+
+    public void setReferant(Employe referant) {
+        this.referant = referant;
+    }
+    
+    
+
+}
