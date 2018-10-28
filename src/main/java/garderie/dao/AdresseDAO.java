197a29610a867d51da35adaec7db1936c4760505
Joseph Lurel <joseph.lurel@gmail.com>
2018-10-28 12:20:21 -0400

    AdresseDAO

--- src/main/java/garderie/dao/AdresseDAO.java
+++ src/main/java/garderie/dao/AdresseDAO.java
@@ -7,6 +7,7 @@ package garderie.dao;
 
 import garderie.model.Adresse;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -65,18 +66,71 @@ public class AdresseDAO extends CommonDAO<Adresse>{
     }
 
     @Override
-    public void delete(Adresse obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(Adresse adresse) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLConstant.DELETE_ADRESSE);
+            
+            preparedStatement.setInt(1, adresse.getIdAdresse());
+            
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            Logger.getLogger(AdresseDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
     @Override
     public Adresse findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Adresse adresse = new Adresse();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLConstant.SELECT_ADRESSE_BY_ID);
+            
+            preparedStatement.setInt(1, id);
+            
+            ResultSet result = preparedStatement.executeQuery();
+            if (result.first()) {
+                adresse.setIdAdresse(id);
+                adresse.setLigne1(result.getString("ligne_1"));
+                adresse.setLigne2(result.getString("ligne_2"));
+                adresse.setLigne3(result.getString("ligne_3"));
+                adresse.setCodePostal(result.getString("codePostal"));
+                adresse.setVille(result.getString("ville"));
+                adresse.setPays(result.getString("pays"));
+            }
+            
+        } catch (SQLException e) {
+            Logger.getLogger(AdresseDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return adresse;
     }
 
     @Override
     public ArrayList<Adresse> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Adresse adresse;
+        ArrayList<Adresse> adresses = new ArrayList<>();
+        try {
+            //Creation of the PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLConstant.SELECT_ADRESSES);
+            
+            //Recupere les resultats de la requete
+            ResultSet result = preparedStatement.executeQuery();
+            
+            //tant qu'il y a une activite dans les resultats
+            while (result.next()) {
+                adresse = new Adresse();
+                adresse.setIdAdresse(result.getInt("adresseId"));
+                adresse.setLigne1(result.getString("ligne_1"));
+                adresse.setLigne2(result.getString("ligne_2"));
+                adresse.setLigne3(result.getString("ligne_3"));
+                adresse.setCodePostal(result.getString("codePostal"));
+                adresse.setVille(result.getString("ville"));
+                adresse.setPays(result.getString("pays"));
+                adresses.add(adresse);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(ActiviteDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return adresses;
     }
     
 }
