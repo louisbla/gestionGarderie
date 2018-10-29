e59ce62bf426866d8e11cffda4b684603fac2325
Youness Dendane <youness.dendane1@uqac.ca>
2018-10-29 11:14:03 -0400

    Ajout du groupe pour un enfant

--- src/main/java/garderie/model/Enfant.java
+++ src/main/java/garderie/model/Enfant.java
@@ -9,25 +9,26 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.sql.Date;
 
-
 public class Enfant extends Personne implements Serializable {
+
     private String photo;   //url
     private DossierInscription dossier;
     private ArrayList<Filiation> listeParents;
     private InventaireEnfant inventaire;
     private ArrayList<Traitement> traitement;
-    
+    private Groupe groupe;
+
     public Enfant() {
     }
 
     //Constructeur
-    public Enfant(String Nom, String Prenom, String Sexe, Date DateNaissance, String numSecu, ArrayList<PersonneAdresse> adresses, String photo,DossierInscription d, ArrayList<Filiation> parents, InventaireEnfant i, ArrayList<Traitement> t) {
+    public Enfant(String Nom, String Prenom, String Sexe, Date DateNaissance, String numSecu, ArrayList<PersonneAdresse> adresses, String photo, DossierInscription d, ArrayList<Filiation> parents, InventaireEnfant i, ArrayList<Traitement> t) {
         super(Nom, Prenom, Sexe, DateNaissance, numSecu, adresses);
         this.photo = photo;
-        this.dossier = d; 
+        this.dossier = d;
         this.listeParents = parents;
         this.inventaire = i;
-        this.traitement = t;      
+        this.traitement = t;
     }
 
     public String getPhoto() {
@@ -69,4 +70,13 @@ public class Enfant extends Personne implements Serializable {
     public void setTraitement(ArrayList<Traitement> traitement) {
         this.traitement = traitement;
     }
+
+    public Groupe getGroupe() {
+        return groupe;
+    }
+
+    public void setGroupe(Groupe groupe) {
+        this.groupe = groupe;
+    }
+
 }
