3e650c2522411dafd605826fc2030214a9cb5b7f
Youness Dendane <youness.dendane1@uqac.ca>
2018-10-29 14:31:13 -0400

    Re ajout de la classe Filiation

---
+++ src/main/java/garderie/model/Filiation.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garderie.model;
+
+public class Filiation {
+
+    private Enfant e;
+    private Parent p;
+    private boolean visible;
+    private String lien;
+
+    public Filiation() {
+
+    }
+
+    public Filiation(Enfant e, Parent p) {
+        this.e = e;
+        this.p = p;
+        this.visible = true;
+    }
+
+    public Enfant getE() {
+        return e;
+    }
+
+    public void setE(Enfant e) {
+        this.e = e;
+    }
+
+    public Parent getP() {
+        return p;
+    }
+
+    public void setP(Parent p) {
+        this.p = p;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public String getLien() {
+        return lien;
+    }
+
+    public void setLien(String lien) {
+        this.lien = lien;
+    }
+
+}
