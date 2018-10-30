2802b93903f5d9a070dee6b41c2a453dfc79a53a
Unknown <maryline.yakan@gmail.com>
2018-10-30 20:47:33 +0100

    CompteUserDAO fini ---> a tester

--- src/main/java/garderie/dao/CompteUserDAO.java
+++ src/main/java/garderie/dao/CompteUserDAO.java
@@ -25,30 +25,144 @@ public class CompteUserDAO extends CommonDAO<CompteUser>{
     }
 
     @Override
-    public CompteUser create(CompteUser obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public CompteUser create(CompteUser compte) {
+        try{
+            //Creation of the PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    SQLConstant.INSERT_COMPTE_USER);
+            
+            //Insert parameter at the location of the question mark in the SQL Query
+            preparedStatement.setString(1, compte.getLogin());
+            preparedStatement.setString(2, compte.getMdp());
+            preparedStatement.setBoolean(3, compte.isPrivilege());
+            preparedStatement.setInt(4, compte.getPersonne().getIdPersonne());
+            
+            System.out.println(preparedStatement.toString());
+
+            //Executing the preparedStatement
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+        }catch (SQLException e) {
+            Logger.getLogger(CompteUserDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return compte;
+    
     }
 
     @Override
-    public CompteUser update(CompteUser obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public CompteUser update(CompteUser compte) {
+        
+        try{
+            //Creation of the PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    SQLConstant.UPDATE_COMPTE_USER); 
+            
+            //Insert parameter at the location of the question mark in the SQL Query
+            preparedStatement.setString(1, compte.getLogin());
+            preparedStatement.setString(2, compte.getMdp());
+            preparedStatement.setBoolean(3, compte.isPrivilege());
+            preparedStatement.setInt(4, compte.getPersonne().getIdPersonne());
+            
+            System.out.println(preparedStatement.toString());
 
+            //Executing the preparedStatement
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            
+        }catch (SQLException e) {
+            Logger.getLogger(CompteUserDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return compte;
+    }
+    
+    
+    
     @Override
-    public void delete(CompteUser obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(CompteUser compte) {
+        try{
+            //Creation of the PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLConstant.DELETE_COMPTE_USER);
+
+            //Insert parameter at the location of the question mark in the SQL Query
+            preparedStatement.setInt(1, compte.getIdUser());
+
+            //Executing the preparedStatement
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            
+        }catch (SQLException e) {
+            Logger.getLogger(CompteUserDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
+    
     @Override
     public CompteUser findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        CompteUser compte = new CompteUser();
+        
+        try{
+             //Creation of the PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLConstant.SELECT_COMPTE_USER_BY_ID);
+
+            //Insert parameter at the location of the question mark in the SQL Query
+            preparedStatement.setInt(1, id);
+            
+            System.out.println(preparedStatement.toString());
+
+            ResultSet result = preparedStatement.executeQuery();  
+            
+            PersonneDAO personneDAO = new PersonneDAO(connection);
+            Personne personne = personneDAO.findById(result.getInt("personneId"));
+            
+            if(result.first()){
+                compte.setIdUser(id);
+                compte.setLogin(result.getString("userId"));
+                compte.setMdp(result.getString("password"));
+                compte.setPrivilege(result.getBoolean("privilege"));
+                compte.setPersonne(personne);
+            }  
+        }catch (SQLException e) {
+            Logger.getLogger(CompteUserDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+    
+        return compte;
     }
 
+    
+    
     @Override
     public ArrayList<CompteUser> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        ArrayList<CompteUser> listecompte = new ArrayList<>();
+        
+        try{
+            //Creation of the PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLConstant.SELECT_COMPTE_USER);
+
+            System.out.println(preparedStatement.toString());
+
+            ResultSet result = preparedStatement.executeQuery();
+            
+            while(result.next()){
+                CompteUser compte = new CompteUser();
+                PersonneDAO personneDAO = new PersonneDAO(connection);
+                Personne personne = personneDAO.findById(result.getInt("personneId"));
+                
+                compte.setIdUser(compte.getIdUser());
+                compte.setLogin(compte.getLogin());
+                compte.setMdp(compte.getMdp());
+                compte.setPrivilege(compte.isPrivilege());
+                compte.setPersonne(personne);
+                
+                listecompte.add(compte);
+            }
+            
+        }catch (SQLException e) {
+            Logger.getLogger(CompteUserDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+    
+        return listecompte;
     }
-    
-    
-    
+
 }
